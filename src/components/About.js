b3dacae46a79731efed8a753b96a93e5020f70a8
John J <john310897@gmail.com>
2025-09-22 15:52:14 +0530

    updating new work example

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 const About = () => {
+  const [count, setCount] = useState(0);
+  const [circleList, setCircleList] = useState([]);
+  const handleOnClick = () => {
+    setCount(count + 1)
+    setCircleList([...circleList, { color: null, id: count }])
+  }
+  const handleCircleOnClick=(index)=>{
+    let tempList=circleList
+    if(tempList[index]?.color===null){
+      tempList[index].color='red';
+    }else{
+      tempList[index].color=null;
+    }
+    
+    setCircleList([...tempList])
+  }
   return (
     <>
-      <p>About page</p>
+      <button onClick={handleOnClick}>click</button>
+      <p>{count}</p>
+      {circleList?.map((circle, index) => (
+        <div className='circle'key={index} style={{backgroundColor:(circle?.color)}} onClick={()=>handleCircleOnClick(index)}>
+        </div>
+      ))}
+
     </>
   );
 };
